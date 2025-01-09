a84cef8ad38e256d8b8e58240352a8c08e200855
priyanshirabadiya <rabadiyapriyansi@gmail.com>
2025-01-09 22:08:47 +0530

    content added

--- app.js
+++ app.js
@@ -11,6 +11,12 @@ const port = process.env.PORT;
 server.use(morgan('dev'));
 server.use(express.json());
 server.use(express.urlencoded({ extended: false }));
+app.use(cors({
+    origin: '*', // Or specify your frontend's URL for better security
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
+    credentials: true,
+}));
 
 server.get('/', (req, res) => {
     res.send("Welcome To Express backend-ecommerce Server...");
